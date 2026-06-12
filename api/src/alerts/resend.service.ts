a96ad0b78d018a98ac8e3cd0cb5064c096702d55
goodnessdevs <devswithgoody82@gmail.com>
2026-06-12 23:14:22 +0100

    new

--- api/src/alerts/resend.service.ts
+++ api/src/alerts/resend.service.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 
 @Injectable()
 export class ResendService {
-    private resend = new Resend(process.env.RESEND_API_KEY);
+    private resend = new Resend(process.env.RESEND_API_KEY!);
 
     async sendEmail(to: string, subject: string, message: string) {
         try {
